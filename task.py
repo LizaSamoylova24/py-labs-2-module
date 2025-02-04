8a64b61c64b301400d2a35ba445dab8ae09f8e45
LizaSamoylova24 <elizaveta.samoylova.05@inbox.ru>
2025-02-04 19:24:37 +0300

    Update task.py

--- task.py
+++ task.py
@@ -1,33 +1,35 @@
-class Animal: #задаем основной класс
-    def __init__(self, name, species): #опредеяем какие будут аргументы
-        self.name = name #создаем атрибут
-        self.species = species #создаем атрибут
+class Animal:  # задаем основной класс
 
-    def sound(self): #метод sound возвращающий значение Unknown sound
+    def __init__(self, name, species):  #опредеяем какие будут аргументы
+        self.name = name  # создаем атрибут
+        self.species = species  # создаем атрибут
+
+    def sound(self) -> str:  # метод sound возвращающий значение Unknown sound
         return "Unknown sound"
 
-    def __str__(self): #метод возвращающий строку в формате "название вид"
+    def __str__(self) -> str:  # метод возвращающий строку в формате "название вид"
         return f"{self.name} the {self.species}"
 
-    def __repr__(self): # метод создающий строку, которую можно использовать для создания экземпляра класса
+    def __repr__(self) -> str:  # метод создающий строку, которую можно использовать для создания экземпляра класса
         return f"{self.__class__.__name__}(name={self.name!r}, species={self.species!r})"
 
-class Bird(Animal): #задаем подкласс (наследуемый)
-    def __init__(self, name, species, wingspan): #конструктор класса с тремя аргументами
-        super().__init__(name, species) #вызываем конструктор базового класса для инициализациии атрибутов name и species
-        self.wingspan = wingspan #создаем атрибут wingspan
+class Bird(Animal):  # задаем подкласс (наследуемый)
 
-    def fly(self): #определяем метод, для возвращения строки
+    def __init__(self, name: str, species: str, wingspan: int):  # конструктор класса с тремя аргументами
+        super().__init__(name, species)  # вызываем конструктор базового класса для инициализациии атрибутов name и species
+        self.wingspan = wingspan  # создаем атрибут wingspan
+
+    def fly(self) -> str:  # определяем метод, для возвращения строки
         return f"{self.name} is flying!"
 
-    def _str_(self): #метод возвращающий строковое представление объекта
+    def __str__(self) -> str:  # метод возвращающий строковое представление объекта
         return f"{self.name} the {self.species} with a wingspan of {self.wingspan} cm"
 
-# Создание объектов
-animal = Animal("Lion", "Feline") #создание объекта
-bird = Bird("Eagle", "Bird of prey", 220) #тоже самое, другой метод
+  # Создание объектов
+animal = Animal("Lion", "Feline")  # создание объекта
+bird = Bird("Eagle", "Bird of prey", 220)  # тоже самое, другой метод
 
-# Вывод информации о животных
+  # Вывод информации о животных
 print(animal)
 print(bird)
 print(bird.fly())
